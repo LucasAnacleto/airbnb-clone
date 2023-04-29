908d0b905c0f8d9b94eddd5934bf80e575fd4ad7
lucas.anacleto <lucas.anacleto@outlook.com>
2023-04-29 13:22:52 -0300

    Navbar - MenuItem and Avatar and UserMenu

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,40 +1,78 @@
 'use client';
 
+import { BiSearch } from 'react-icons/bi';
+
 const Search = () => {
     return (
         <div
             className="
-                border-[1px]
-                w-full
-                md:w-auto
-                py-2
-                rounded-full
-                shadow-sm
-                hover:shadow-md
-                transition
+                border-[1px] 
+                w-full 
+                md:w-auto 
+                py-2 
+                rounded-full 
+                shadow-sm 
+                hover:shadow-md 
+                transition 
                 cursor-pointer
             "
         >
             <div
                 className="
-                    flex
-                    flex-row
-                    items-center
+                    flex 
+                    flex-row 
+                    items-center 
                     justify-between
                 "
             >
                 <div
                     className="
-                        text-sm
-                        font-semibold
+                        text-sm 
+                        font-semibold 
                         px-6
                     "
                 >
-                    anywhere
-                    
+                    Qualquer lugar
+                </div>
+                <div
+                    className="
+                        hidden 
+                        sm:block 
+                        text-sm 
+                        font-semibold 
+                        px-6 
+                        border-x-[1px] 
+                        flex-1 
+                        text-center
+                    "
+                >
+                    Qualquer semana
+                </div>
+                <div
+                    className="
+                        text-sm 
+                        pl-6 
+                        pr-2 
+                        text-gray-600 
+                        flex 
+                        flex-row 
+                        items-center 
+                        gap-3
+                    "
+                >
+                    <div className="hidden sm:block">Hóspedes?</div>
+                    <div
+                        className="
+                        p-2 
+                        bg-rose-500 
+                        rounded-full 
+                        text-white
+                        "
+                    >
+                        <BiSearch size={18} />
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 }
